d24d76a9fd450635baf1f8ac38595c0174ae9c06
wayde191 <wwsun@thoughtworks.com>
2016-08-25 17:30:49 +0800

    Modified: process company json string; Wayde

--- src/fund_service/config.clj
+++ src/fund_service/config.clj
@@ -5,6 +5,8 @@
 (defn east-fund-service-url []
   (env "EAST_FUND_SERVICE_URL"))
 
+(def fund-code-url "http://fund.eastmoney.com/js/fundcode_search.js")
 
+(def fund-company-url "http://fund.eastmoney.com/api/static/FundCommpanyInfo.js")
 
 
--- src/fund_service/fund.clj
+++ src/fund_service/fund.clj
@@ -7,7 +7,26 @@
             [environ.core  :refer [env]]
             [fund-service.config :as config]
             [clojure.tools.logging :as log]
+            [pl.danieljanus.tagsoup :as html-parser]
             ))
 
+(defn http-atom [request]
+  (client/with-middleware client/default-middleware
+    (:body (client/get (request :url)
+             request))))
+
+(defn process-fund-company []
+  (try
+    (let [html-str (http-atom {:url config/fund-company-url})
+          compaines (json/read-str (string/replace html-str #"﻿var FundCommpanyInfos=" "") :key-fn keyword)]
+      (println (count (map #(get % :_id) compaines))))
+    (catch Exception e
+      (log/error (str "caught exception: " (.getMessage e) " with process-fund-company")))))
+
 (defn start []
-  (log/info "Starting the fund service ... "))
+  (log/info "Starting the fund service ... ")
+  (process-fund-company))
+
+
+
+;  (println (get-in (html-parser/parse config/fund-company-url) [3 2])))
